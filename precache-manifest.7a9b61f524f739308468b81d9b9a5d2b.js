20664606a92593833e066632b8ddb13cf8297c77
super-marios <wjckorea001@gmail.com>
2020-06-22 12:01:40 +0900

    Updates

---
+++ precache-manifest.7a9b61f524f739308468b81d9b9a5d2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8174290d0ce244aeb930e111cf39276",
+    "url": "/react-gtm-test/index.html"
+  },
+  {
+    "revision": "b5443ced45756dd06135",
+    "url": "/react-gtm-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "32feeca589ae38c90fce",
+    "url": "/react-gtm-test/static/js/2.f97afa59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gtm-test/static/js/2.f97afa59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5443ced45756dd06135",
+    "url": "/react-gtm-test/static/js/main.cf506fc4.chunk.js"
+  },
+  {
+    "revision": "0f3a79719beb3ea745a1",
+    "url": "/react-gtm-test/static/js/runtime-main.c3e034db.js"
+  }
+]);
