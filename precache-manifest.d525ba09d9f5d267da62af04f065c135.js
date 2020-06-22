82f4deec14ee5367c9083fa71382f6416d47af2b
super-marios <wjckorea001@gmail.com>
2020-06-22 14:56:24 +0900

    Updates

--- precache-manifest.d525ba09d9f5d267da62af04f065c135.js
+++ precache-manifest.d525ba09d9f5d267da62af04f065c135.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "18568750e5345a9fcc299b076a1c5b4c",
+    "revision": "3e9f72190ddc7a9c8bce5ace2bb62540",
     "url": "/react-gtm-test/index.html"
   },
   {
-    "revision": "ddd46fe22245451f7807",
+    "revision": "3086ca9ffdc0d606bae6",
     "url": "/react-gtm-test/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "d21a179d764015af92ce",
-    "url": "/react-gtm-test/static/js/2.71b00336.chunk.js"
+    "revision": "62023027063f1c3e4829",
+    "url": "/react-gtm-test/static/js/2.c7e773ed.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react-gtm-test/static/js/2.71b00336.chunk.js.LICENSE.txt"
+    "url": "/react-gtm-test/static/js/2.c7e773ed.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ddd46fe22245451f7807",
-    "url": "/react-gtm-test/static/js/main.1533ad8d.chunk.js"
+    "revision": "3086ca9ffdc0d606bae6",
+    "url": "/react-gtm-test/static/js/main.e88c8668.chunk.js"
   },
   {
     "revision": "0f3a79719beb3ea745a1",
