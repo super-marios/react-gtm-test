98ab2d44c0e0eb18413827be01fc7570644cdd16
super-marios <wjckorea001@gmail.com>
2020-07-03 18:06:03 +0900

    Updates

--- precache-manifest.f21b064488741af3ae471bedf9c34e92.js
+++ precache-manifest.f21b064488741af3ae471bedf9c34e92.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "46ae899944b9b2c2cf7f71a4651ae0f3",
+    "revision": "eebd00ede959628deaac77374818b0ef",
     "url": "/react-gtm-test/index.html"
   },
   {
-    "revision": "52e6a9311ff1f2c1631d",
+    "revision": "d3ee1852c1fd66a3c95c",
     "url": "/react-gtm-test/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-gtm-test/static/js/2.b50d5016.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "52e6a9311ff1f2c1631d",
-    "url": "/react-gtm-test/static/js/main.95081392.chunk.js"
+    "revision": "d3ee1852c1fd66a3c95c",
+    "url": "/react-gtm-test/static/js/main.674f4c10.chunk.js"
   },
   {
     "revision": "0f3a79719beb3ea745a1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gtm-test/precache-manifest.467545b7459fb9ed1918fd64dd08d58c.js"
+  "/react-gtm-test/precache-manifest.f21b064488741af3ae471bedf9c34e92.js"
 );
 
 self.addEventListener('message', (event) => {
